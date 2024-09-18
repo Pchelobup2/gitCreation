8d913e0b68e7956d1ccf8578464fb4acb77dc40d
alina-gasprom <alina.k.91@icloud.com>
2024-09-18 18:43:08 +0300

    start

---
+++ src/main/java/ru/alina/gitcreation/GitCreationApplication.java
@@ -0,0 +1,13 @@
+package ru.alina.gitcreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCreationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitCreationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alina/gitcreation/GitCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alina.gitcreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCreationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
