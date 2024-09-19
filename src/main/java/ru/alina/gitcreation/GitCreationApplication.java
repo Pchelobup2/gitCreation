61dc525be49983ac2d3b5a64e99413349fda11c0
alina-gasprom <alina.k.91@icloud.com>
2024-09-19 15:22:42 +0300

    sout f4

--- src/main/java/ru/alina/gitcreation/GitCreationApplication.java
+++ src/main/java/ru/alina/gitcreation/GitCreationApplication.java
@@ -8,7 +8,6 @@ public class GitCreationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitCreationApplication.class, args);
-        System.out.println("feature 2");
         System.out.println("feature 1");
         System.out.println("feature 2");
         System.out.println("feature 3");
